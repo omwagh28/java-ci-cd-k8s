16a9b315ec4aee3448d99ae85f71637a74f537a6
omwagh28 <annasaheb.23bcs10142@ms.sst.scaler.com>
2026-01-18 19:16:50 +0530

    Change version to 10

--- src/main/java/com/example/demo/HealthController.java
+++ src/main/java/com/example/demo/HealthController.java
@@ -8,6 +8,6 @@ public class HealthController {
 
     @GetMapping("/health")
     public String health() {
-        return "VERSION_RAM_Shyam_maratha";    
+        return "VERSION 10";    
     }
 }
